a0c35bb9fd0b78ad30ad1baacf3cea9b6e08e227
unknown <shindeabhishekm@gmail.com>
2024-12-18 14:53:18 +0530

    day3 changes 1

--- src/main/java/PodTest.java
+++ src/main/java/PodTest.java
@@ -2,7 +2,17 @@ public class PodTest {
 
  
     public static void main(String [] args)  {
-        System.out.println("Hello SDET's ");
+        System.out.println("My name is:Abhishek Shinde");
+        PodTest p1 = new PodTest();
+        p1.sum();
+
     }
+    public void sum(){
+        int c = 5+6;
+        System.out.println("Sum is "+c);
+    }
+
+
+
 }
 
