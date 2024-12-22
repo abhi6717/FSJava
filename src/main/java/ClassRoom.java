09513ae95358619c50896d2836c232e4ddfa1b85
unknown <shindeabhishekm@gmail.com>
2024-12-22 17:51:29 +0530

    day5 changes

--- src/main/java/ClassRoom.java
+++
@@ -1,9 +0,0 @@
-public class ClassRoom {
-
-    int floorNumber;
-    int seatCount;
-
-    public void clean(){
-        System.out.println("All the "+seatCount+"are cleaned now!");
-    }
-}
--- src/main/java/Teacher.java
+++
@@ -1,9 +0,0 @@
-public class Teacher {
-
-    String name;
-    String subject;
-
-    public void teach(){
-        System.out.println("My name is "+name+ "& I teach subject: "+subject);
-    }
-}
