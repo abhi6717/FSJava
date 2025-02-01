69d140eaaf57d08438e616e0d9fb177c2021cb12
unknown <shindeabhishekm@gmail.com>
2025-02-01 16:24:45 +0530

    day13 changes

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,20 +1,10 @@
 public class Dog {
 
-    Collar c;
-    String name;
-    public static void main(String[] args) {
 
-        Dog d;
-        d = new Dog();
-        d.go(d);
+
+        String name;
+
+        public void getDetails() {
+            System.out.println(name);
+        }
     }
-
-   public void go(Dog dog){
-
-    c= new Collar();
-    dog.setName("Aiko");
-   }
-     public void setName(String dogName) {
-        name= dogName;
-     }
-     }
---
+++ src/main/java/InheritanceDemo.java
@@ -0,0 +1,27 @@
+public class InheritanceDemo {
+
+        public static void main(String[] args) {
+
+            Dog d = new Dog();
+
+            Puppy p1 = new Puppy();
+
+
+
+            System.out.println(d.name);
+            d.getDetails();
+
+
+            Puppy p = new Puppy();
+            System.out.println(p.name);
+            p.getDetails();
+
+            Object o1 = new Object();
+
+
+
+
+        }
+
+    }
+
